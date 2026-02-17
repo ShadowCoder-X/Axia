76b0ecc8e7156b61a45e13e82eeb367ec0973137
omoladeajala <shadowcoderx.02@gmail.com>
2026-02-18 00:41:19 +0100

    Fixed nested repo and seasons logic

---
+++ Loop/script.js
@@ -0,0 +1,39 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+// for (let i = 1; i < 5; i = i + 1) {
+//  console.log("Numbers");
+// }
+
+// for (let i = 1; i <= 11; i = i + 1) {
+//  console.log(`Number ${i}`);
+//}
+
+// for (let i = 10; i >= 5; i = i - 1) {
+//  console.log(`Number ${i}`);
+//}
+
+// for (let i = 1; i < 21; i = i + 2) {
+//  console.log(`Odd ${i}`);
+//}
+
+// for (let i = 0; i < 21; i = i + 2) {
+//  console.log(`Even ${i}`);
+//}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 1; i <= numbers.length; i++) {
+  console.log(i);
+}
+
+let names = ["John", "Jane", "Doe"];
+console.log(names[0]);
+console.log(names[1]);
